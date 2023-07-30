dd7799b900cf65c41fa7e9011a45559c83881353
LSH <121619411+Choguesung@users.noreply.github.com>
2023-07-30 17:26:47 +0900

    Update App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 
 function App() {
  
-  let [글제목,글제목변경] = useState(['여자 코트 추천','남자 코트 추천','강남 우동 맛집']); // 얘는 자동으로 렌더링 돼서 변경이 됨 html이 >> 이게 반응형인가
-  let [따봉,따봉변경] = useState(0);
-  let [modal,setModal] = useState(false);
+  let [글제목, 글제목변경] = useState(['여자 코트 추천','남자 코트 추천','강남 우동 맛집']); // 얘는 자동으로 렌더링 돼서 변경이 됨 html이 >> 이게 반응형인가
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     
@@ -14,49 +16,52 @@ function App() {
         <h4> Blog </h4>
       </div>
 
-      <button onClick={ ()=>{
-        let copy = [...글제목]; // 포인터를 새로 만들어주세요 
-        copy[0] = '여자 코트 추천'
-        글제목변경(copy);
-      }}>
-        글수정 </button>
+        {
+          글제목.map(function(a,i){
+            return (
+              <div className="flex flex-col pl-5 justify-start border-b-2 border-gray-500 p-5" onClick={() => {setModal(true),setTitle(i)}}>
+               <div className="flex font-bold pb-5">{ 글제목[i] }
+                 <div onClick={()=> {따봉변경(따봉+1)}}> 👍 {따봉}</div>
+               </div>
+               <p> 2월 17일 발행</p>
+               <button className="border border-red-500 w-10"
+               onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+               }}>삭제</button>
+              </div>
+            )
+          })
+        }
 
-      <div className="pl-5 justify-start border-b-2 border-gray-500 p-5">
-         <div className="flex font-bold pb-5">
-            <div className="pr-3"> { 글제목[0] } </div>
-            <div className="pr-1"> <button onClick={() => {따봉변경(따봉++)}}> 👍 </button> </div>
-             {따봉} 
-         </div>
-         <div>2월 17일 발행</div>
-        </div>
+        <input onChange={(e) => { 
+          입력값변경(e.target.value);
+        }}></input>
+        <button onClick={() => {
+          let copy = [...글제목];
+          copy.unshift(입력값);
+          글제목변경(copy);
+        }}>글발행</button>
+        
+        {
+           modal == true ? <Modal 글제목={글제목} title={title} ></Modal> : null
+        }
 
-        <div className="flex flex-col pl-5 justify-start border-b-2 border-gray-500 p-5">
-          <div className="font-bold pb-5">{ 글제목[1] }</div>
-          <div>2월 17일 발행</div>
-        </div>
-
-        <div className="flex flex-col pl-5 justify-start border-b-2 border-gray-500 p-5">
-          <div className="font-bold pb-5" >{ 글제목[2] }</div>
-          <div onClick={() => {setModal(true);}} >2월 17일 발행임</div>
-        </div>
-
-         {
-            modal == true ? <Modal></Modal> : null
-         }
-         
-
-      </div>
+       </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
-    <div className="p-5 bg-gray-300 text-start">
-          <div className="font-bold">제목</div>
+    <div className="p-5 bg-gray-300 text-start bg-gray-500">
+          <div className="font-bold">{props.글제목[props.title]}</div>
           <div>날짜</div>
           <div>내용</div>
     </div>
   )
 }
+
+
 
 export default App;
