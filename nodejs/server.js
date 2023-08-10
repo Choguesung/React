5ada0856a3687277b040632025c5aa9aa1b30195
LSH <121619411+Choguesung@users.noreply.github.com>
2023-08-10 22:26:32 +0900

    nodejs

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// 몽고 db 연결
+app.use(express.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient
-MongoClient.connect('mongodb+srv://admin:<qwer1234>@cluster0.gnxrofg.mongodb.net/?retryWrites=true&w=majority', function (에러, client) {
-  app.listen(8080, function () {
-    console.log('listening on 8080')
-  });
-})
+app.set('view engine','ejs')
 
+var db;
+
+MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.gnxrofg.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
+  if (에러) return console.log(에러)
+
+  db = client.db('todoapp');
+
+  db.collection('post').insertOne({이름:'John',나이: 20},function(에러,결과){
+    console.log('저장완료');
+  });
+
+  app.listen(8080, function() {
+    console.log('listening on 8080')
+  })
+})
 
 
 app.get('/', function (요청, 응답) {
@@ -24,5 +32,13 @@ app.get('/write', function (요청, 응답) {
 app.post('/add', function (요청, 응답) {
   console.log(요청.body.title)
   console.log(요청.body.date)
-  응답.send('전송완료')
+  db.collection('post').insertOne( { 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
+    console.log('저장완료')
+  });
 });
+
+app.get('/list',function(요청,응답){
+  응답.render('list.ejs');
+});
+
+
